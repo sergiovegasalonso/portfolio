5d52fa5408fbe600c0c740010f73863643d5e8fd
sergio <sergio.vegas.alonso@gmail.com>
2022-06-16 01:51:45 +0200

    FONt

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,23 @@
 <script setup lang="ts"></script>
 
-<template></template>
+<template><h1>holaaa</h1></template>
 
-<style></style>
+<style>
+@import url("https://fonts.googleapis.com/css2?family=Lato&family=Roboto+Mono:wght@100&display=swap");
+
+* {
+  font-family: "Roboto Mono", monospace;
+}
+
+html,
+body {
+  box-sizing: content-box;
+}
+
+body {
+  margin: 0;
+  width: 100%;
+  height: 100vh;
+  background: #333;
+}
+</style>
