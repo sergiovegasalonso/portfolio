1d6bce22d60fd700558580acb4b5d155660ceac5
sergio <sergio.vegas.alonso@gmail.com>
2022-06-19 01:47:37 +0200

    Tailwind added.

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
 
 <template>
   <h1>holaaa</h1>
+  <h1 class="text-3xl font-bold underline">Hello world!</h1>
   <router-link to="/">Go to Home</router-link>
   <router-link to="/about">Go to About</router-link>
   <router-view></router-view>
